a1017fa13a7d62abc5822f65682fcd3cb07e0cb4
Mangaljyotijena <55190271+Mangaljyotijena@users.noreply.github.com>
2021-08-04 22:06:21 +0530

    action update

--- src/action.js
+++ src/action.js
@@ -43,8 +43,7 @@ async function run() {
     ...context.owner,
     ...context.repo,
     issue_number: pull_request.number,
-    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.
-    \n\n${ResonseData}`,
+    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n${ResonseData}`,
   });
 }
 
